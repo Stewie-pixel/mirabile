48c27e8f7775526c7c6595691fb2883bf86702ef
Chi Vi Han <hanchivi39@gmail.com>
2026-05-22 23:34:21 +1000

    ci: add jenkins pipeline configured with docker (#71)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
+import './lib/datadog';
 
 createRoot(document.getElementById("root")!).render(
   <AppWrapper>
